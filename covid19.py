ccb17ffd4637c415cb63f50cfa1ece1c20aaeb30
Timothy Ganoza <38800640+jostimian@users.noreply.github.com>
2020-04-10 14:50:26 +0800

    Added CLI call outs for debugging

--- covid19.py
+++ covid19.py
@@ -1,7 +1,21 @@
+import datetime # So the user can see what time did he/she update the covid19 lists
+import time # I use time.sleep to make stop CPU throttling for very slow systems
+print("Importing Tkinter")# I add This Callouts so the user can understand what is happening in the background 
 from tkinter import *
+time.sleep(1)
+print("Tkinter Imported")
+print("Importing Tkinter Messagebox")
 from tkinter import messagebox
+time.sleep(1)
+print("Tkinter Messagebox Imported")
+print("Importing COVID19Py")
 import COVID19Py
+time.sleep(1)
+print("COVID19Py Imported")
+print("Importing Pandas")
 import pandas as pd
+time.sleep(1)
+print("Pandas Imported")
 def fight_corona(conf_list):
     d = pd.DataFrame.from_dict(conf_list)
     d = pd.concat([d, d['coordinates'].apply(pd.Series),d['latest'].apply(pd.Series)], axis=1)
@@ -12,34 +26,49 @@ def confirmed_wrld():
     conf_list = covid19.getLocations(rank_by='confirmed')
     df = fight_corona(conf_list)
     df.to_excel('confirmed.xlsx')
+    print("Confirmed.xlsx is updated on " + str(datetime.datetime.now()))
     messagebox.showinfo('Saved','Saved In confirmed.xlsx')
 def ph():
-    messagebox.showinfo("Warning","This Might Take A Min")
+    messagebox.showinfo("Wait","This may take a few minutes")
     location = covid19.getLocationByCountryCode("PH")
     df = fight_corona(location)
-    df.to_excel('PH.xlsx')
+    df.to_excel('ph.xlsx')
+    print("Ph.xlsx is updated on " + str(datetime.datetime.now()))
     messagebox.showinfo('Saved','Saved in PH.xlsx')
 def find():
-    messagebox.showinfo("Warning","This Might Take A Min Or More")
+    messagebox.showinfo("Wait","This may take a few minutes")
     code = find_Entry.get()
-    Find_Location = covid19.getLocationByCountryCode(code)
-    df = fight_corona(Find_Location)
+    findLocation = covid19.getLocationByCountryCode(code)
+    df = fight_corona(findLocation)
     df.to_excel(code + ".xlsx")
+    print(code + ".xlsx is updated on " + str(datetime.datetime.now()))
     messagebox.showinfo("Saved", "Saved in" + code +".xlsx")
+
+print("Connecting In Johns Hopkins University's(jhu) Database")
 covid19 = COVID19Py.COVID19(data_source="jhu")
+print("This may take a few seconds depending on your internet connection")
+time.sleep(1)
+print("Connection Done")
+print("exiting CLI")
+print("Opening GUI")
 root = Tk()
+messagebox.showinfo("Special Thanks","Special Thanks For The FrontLiners Out There Trying To Find Cure And Helping Serve Their Country And The World #BeatCovid19!")
 root.geometry("400x300")
 root.title("CoronaVirusLocator Version 1.3")
 
-confirmed_BTN = Button(text="confirmed cases death and recoveries of COVID19", command = confirmed_wrld)
-confirmed_BTN.pack()
+confirmedBtn = Button(text="confirmed cases death and recoveries of COVID19", command = confirmed_wrld)
+confirmedBtn.pack()
 
-ph_BTN = Button(text = "Covid19 Cases in the PH",command = ph)
-ph_BTN.pack()
+phBtn = Button(text = "Covid19 Cases in the PH",command = ph)
+phBtn.pack()
 
 find_Entry = Entry()
-find_Entry.pack()
+find_Entry.pack() 
 
-find_btn = Button(text = "Search By Country Code eg.(PH)", command = find)
-find_btn.pack()
+findBtn = Button(text = "Search By Country Code eg.(PH)", command = find)
+findBtn.pack()
 root.mainloop()
+print("Quiting")
+print("Thanks For Using This Application ❤😊😊")
+time.sleep(2)
+print("Confirmed Quit")
